2a13bd2d8c2402316b23ce381bd33340ababc739
Oleg Orykhivskiy <olegkoolll@gmail.com>
2025-01-17 20:09:12 +0200

    Done

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,21 @@
 'use strict';
 
-// write code here
+const list = document.querySelector('ul');
+
+const parseSalary = (salary) => parseFloat(salary.replace(/[$,]/g, ''));
+// eslint-disable-next-line no-shadow, no-unused-vars
+const sortList = (list) => {
+  const items = Array.from(list.children);
+
+  items.sort((a, b) => {
+    const salaryA = parseSalary(a.dataset.salary);
+    const salaryB = parseSalary(b.dataset.salary);
+
+    return salaryB - salaryA;
+  });
+
+  items.forEach((item) => list.appendChild(item));
+};
+
+// eslint-disable-next-line no-undef
+sortList(list);
